8341be6e17e67cd41caf0a8b0a87792ba2257d42
AlexSvet117 <alex.svet117@gmail.com>
2025-03-16 22:34:18 +0000

    03 solution

--- 03_secret_message_decoder/solution.py
+++ 03_secret_message_decoder/solution.py
@@ -2,6 +2,13 @@
 def main():
     pass # remove this line with youir code
 
+text = input("Enter the encoded message:")
+step = input("Enter the step size:")
+
+substring = text[::int(step)]
+
+print(f"Decoded Message: {substring}")
+
 # do not change the following lines:    
 if __name__ == "__main__":
     main()
