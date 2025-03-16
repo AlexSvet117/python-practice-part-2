93206528ee5d1183e24cc1bba73b0cd87dfbfbbd
AlexSvet117 <alex.svet117@gmail.com>
2025-03-16 22:43:56 +0000

    04 solution

--- 04_email_validator/solution.py
+++ 04_email_validator/solution.py
@@ -2,5 +2,21 @@
 def main():
     pass # replace this line with your code
 
+email_address = input("Enter an email address:")
+
+if "@" not in email_address:
+    print("Email must contain an @ symbol")
+elif email_address.count("@") > 1:
+    print("Email must contain only one @ symbol")
+elif email_address[0] == "@":
+    print("Email must have characters before the @ symbol")
+elif "." not in email_address[email_address.find("@"):]:
+    print("Email must have a domain with a period after the @ symbol")
+elif " " in email_address:
+    print("Email cannot contain spaces")
+else: 
+    print("Valid email address!")
+
+
 # do not change the following lines:
 main()
