6aa3e977e33af2095cf6525dbb473c4f638d0a7e
AlexSvet117 <alex.svet117@gmail.com>
2025-03-16 16:14:56 +0000

    01 and 02 solution

--- 01_augumented_assignements/solution.py
+++ 01_augumented_assignements/solution.py
@@ -6,15 +6,15 @@ def main():
     print(f"Current Balance: ${balance:.1f}")
     # write your code here with 4 space intentation
     deposit = input("How much do you want to deposit: $")
-    deposit_int = int(deposit)
-    balance =  balance + deposit_int
+    deposit_int = float(deposit)
+    balance += deposit_int
     print(f"Balance Successfully Updated: ${balance}")
     user_withdrowal = input("How much do you want to withdraw: $")
     print("There is a 3% transaction fee on the withdrawal.")
-    user_withdrowal_int = int(user_withdrowal)
+    user_withdrowal_int = float(user_withdrowal)
     withdrowal_fee = (user_withdrowal_int * 0.03)
     print(f"Withdrawal: ${user_withdrowal_int} - Fee: ${withdrowal_fee}")
-    balance = balance - (user_withdrowal_int + withdrowal_fee)
+    balance -= user_withdrowal_int + withdrowal_fee
     print(f"Balance Successfully Updated: ${balance}")
     print("")
     print("============ TRANSACTION COMPLETED ============")
--- 02_phone_number_formatter/solution.py
+++ 02_phone_number_formatter/solution.py
@@ -1,8 +1,15 @@
 # do not change the method name
 def main():
     # your solution goes here
+    # your solution goes here
     pass # remove this line with your code, this is here just to have the IDE not complaining
 
+phone_number = input("Enter a 10-digit phone number, no spaces, special characters or spaces:")
+
+correct = len(phone_number) == 10 and phone_number.isdigit()
+correct or print("Error: Please enter exactly 10 digits. No spaces, special characters and alphabetical characters.") or exit()
+
+print(f"Formatted Phone Number: ({phone_number[:3]}) {phone_number[3:6]}-{phone_number[6:]}")
 # do not change the code below
 if __name__ == "__main__":
     main()
